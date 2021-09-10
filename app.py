afc9f4db1efaacd1e3cbd289ef21b8634f8a2563
Vaibhavp888 <pvaibhav888@gmail.com>
2021-09-10 19:54:53 +0530

    day2 adding route like create and fetching data to id

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'AOP'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/people_info'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost/peopl_info'
 
 db = SQLAlchemy(app)
 
@@ -16,6 +16,26 @@ class Peoples(db.Model):
     last_name=db.Column('last_name', db.String(50), index=True, unique=True)
     email=db.Column('email',db.String(28), unique=True)
 
+    def __repr__(self):
+        return {
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+            'email':self.email
+        }
+
+# data = [
+#     {"id": 1,
+#     "firstname":"Vaibhav",
+#     "lastname":"Pawar",
+#     "email":"pvai99@gmail.com"
+#     },
+#     {
+#     "id": 2,
+#     "firstname":"Roki",
+#     "lastname":"Barbosa",
+#     "email":"baro9@gmail.com"
+#     }
+# ]
 
 
 @app.route('/')
@@ -26,13 +46,32 @@ def home():
 @app.route('/fetch_user/<people_id>', methods=['GET'])
 def fetch_user(people_id):
     peoples = Peoples.query.get(int(people_id))
+    response = peoples.__repr__()
+    return jsonify(response)
+
+
+
+
+@app.route('/get', methods=['GET'])
+def get_user():
+    peoples = Peoples.data.all()
     return jsonify(peoples)
+
 
 
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
-    return ' yaah! yr created, enjoy! '
+        request_data = request.get_json()
+        people = Peoples(
+            first_name=request_data['first_name'],
+            last_name=request_data['last_name'],
+            email=request_data['email']
+            )
+        db.session.add(people)
+        db.session.commit()
+        
+        return 'People ID has been created'
 
 
 
